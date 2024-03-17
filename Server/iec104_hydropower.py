95e0ae6109b7b8d101a7fa36007a2b2a06224007
feirik <feirik@gmail.com>
2024-03-17 12:54:48 -0600

    Delay grid power in startup

--- Server/iec104_hydropower.py
+++ Server/iec104_hydropower.py
@@ -136,7 +136,7 @@ class IEC104Server:
         self.ioa_register[EXCITE_SWITCH] = 1
         time.sleep(25)
         self.ioa_register[TRANSFORMER_SWITCH] = 1
-        time.sleep(2)
+        time.sleep(3)
         self.ioa_register[GRID_SWITCH] = 1
         self.ioa_register[START_PROCESS] = 0
 
